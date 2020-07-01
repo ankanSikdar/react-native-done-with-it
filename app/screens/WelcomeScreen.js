6a90332355b22f458af724b31412184eff02e188
Ankan Sikdar <ankansikdar@gmail.com>
2020-07-02 01:10:20 +0530

    Applied Styles to WelcomeScreen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -2,19 +2,23 @@ import React from "react";
 import { View, ImageBackground, StyleSheet, Image, Text } from "react-native";
 
 import colors from "../config/colors";
+import AppButton from "../components/AppButton";
 
 const WelcomeScreen = (props) => {
   return (
     <ImageBackground
+      blurRadius={3}
       source={require("../assets/background.jpg")}
       style={styles.background}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 };
@@ -34,15 +38,14 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
   },
-  loginButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: colors.primary,
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
-  registerButton: {
+  buttonContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: colors.secondary,
   },
 });
 
