01c9f5af39ffd2cce359333fab1574b3cde01523
Ankan Sikdar <ankansikdar@gmail.com>
2020-07-15 20:16:49 +0530

    Added Card and ListItem Components and ListingDetailsScreen

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ListingDetailsScreen />;
 }
