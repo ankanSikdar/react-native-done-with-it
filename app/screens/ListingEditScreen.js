7220940fafccf52539edfe1c2e0f507394c60b42
Ankan Sikdar <ankansikdar@gmail.com>
2020-07-21 17:10:38 +0530

    Added FormImagePicker Component

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -9,12 +9,14 @@ import AppFormPicker from "../components/AppFormPicker";
 import AppFormField from "../components/AppFormField";
 import SubmitButton from "../components/SubmitButton";
 import CategoryPickerItem from "../components/CategoryPickerItem";
+import FormImagePicker from "../components/FormImagePicker";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image"),
 });
 
 const categories = [
@@ -83,10 +85,12 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
+          images: [],
         }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
+        <FormImagePicker name="images" />
         <AppFormField maxLength={255} name="title" placeholder="Title" />
         <AppFormField
           keyboardType="numeric"
