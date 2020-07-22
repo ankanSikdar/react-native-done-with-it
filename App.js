1985fd1c10a820533c800745881903194b469f3e
Ankan Sikdar <ankansikdar@gmail.com>
2020-07-22 18:25:07 +0530

    Added AuthNavigation and RegisterScreen

--- App.js
+++ App.js
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  StyleSheet,
-  ImageComponent,
-  TextInput,
-  Text,
-  Switch,
-  Button,
-  Image,
-} from "react-native";
-import * as ImagePicker from "expo-image-picker";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+  );
 }
